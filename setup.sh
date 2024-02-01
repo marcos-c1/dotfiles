40b8c825912c0ee3d5f7c2e11d82c2cb57100350
marcos-c1 <marcos.b.campos14@gmail.com>
2024-02-01 11:32:55 -0300

    feat(sh): adding fn to control files

--- setup.sh
+++ setup.sh
@@ -6,12 +6,40 @@ tmux='tmux.conf'
 zsh='zshrc'
 bash='bashrc'
 rc='rc'
+arg=$1
 
-cp $tmux ~/.$tmux
-cp $zsh ~/.$zsh
-cp $bash ~/.$bash
-cp $rc ~/.ssh/$rc
+new_changes(){
+	cp ~/.$tmux $tmux
+	cp ~/.$zsh $zsh 
+	cp ~/.$bash $bash 
+	cp ~/.ssh/$rc $rc
+}
 
-tmux source-file ~/.$tmux
-source ~/.$zsh
-source ~/.$bash
+helper(){
+	echo " -c \t Copy all the changes made in dotfiles at root directory to this repository\n -s \t Set all dotfiles in root directory\n -h \t Helper."
+}
+
+setup(){
+	cp $tmux ~/.$tmux
+	cp $zsh ~/.$zsh
+	cp $bash ~/.$bash
+	cp $rc ~/.ssh/$rc
+
+	tmux source-file ~/.$tmux
+	source ~/.$zsh
+	source ~/.$bash
+}
+
+if [ "$#" -eq 0 ]; then
+ 	echo "Pass the params to file.\nUse --h to know which flags are disponible."
+ else
+ 	if [ $arg = "--h" ]; then
+ 		helper
+ 	elif [ $arg = "-c" ]; then
+ 		new_changes
+ 	elif [ $arg = "-s" ]; then
+ 		setup
+ 	else
+ 		echo "Invalid argument.\nUse --h to know which flags are disponible."
+ 	fi
+fi
